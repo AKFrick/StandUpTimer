09d5203a1ed3c0f78fb416942d1d1ea1c555ba4f
AKFrick <ak.frick@outlook.com>
2017-12-02 16:24:57 +0500

    try to add sound

--- app/src/main/java/com/akf/standuptimer/MainActivity.java
+++ app/src/main/java/com/akf/standuptimer/MainActivity.java
@@ -1,11 +1,16 @@
 package com.akf.standuptimer;
 
+import android.app.Activity;
+import android.content.Context;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.io.IOException;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -30,14 +35,16 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 timer = new Timer();
-                myTimerTask = new MyTimerTask(textView);
+                myTimerTask = new MyTimerTask(textView, MainActivity.this);
                 timer.schedule(myTimerTask, 5000);
             }
         });
     }
     class MyTimerTask extends TimerTask {
         private TextView textView;
-        MyTimerTask(TextView textView){
+        Activity activity;
+        MyTimerTask(TextView textView, Activity activity){
+            this.activity = activity;
             this.textView = textView;
         }
         @Override
@@ -46,7 +53,7 @@ public class MainActivity extends AppCompatActivity {
             runOnUiThread(new Runnable(){
                 @Override
                 public void run(){
-                    textView.setText("Работает!");
+
                 }
             });
         }
